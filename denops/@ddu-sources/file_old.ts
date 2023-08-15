c10f2744739341554c595064a6827d293690bd5a
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-08-15 10:33:35 +0900

    Format

--- denops/@ddu-sources/file_old.ts
+++ denops/@ddu-sources/file_old.ts
@@ -1,7 +1,4 @@
-import {
-  BaseSource,
-  Item,
-} from "https://deno.land/x/ddu_vim@v3.5.0/types.ts";
+import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.5.0/types.ts";
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.5.0/deps.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.3/file.ts";
 import { relative } from "https://deno.land/std@0.198.0/path/mod.ts";
@@ -17,20 +14,13 @@ export class Source extends BaseSource<Params> {
   }): ReadableStream<Item<ActionData>[]> {
     return new ReadableStream({
       async start(controller) {
-        // Note: rviminfo! is broken in Vim8 before 8.2.2494
-        if (
-          await fn.has(args.denops, "nvim") ||
-          await fn.has(args.denops, "patch-8.2.2494")
-        ) {
-          await args.denops.cmd("wviminfo | rviminfo!");
-        }
+        await args.denops.cmd("wviminfo | rviminfo!");
 
         const cwd = await fn.getcwd(args.denops) as string;
 
-        const oldfiles =
-          (await args.denops.call(
-            "ddu#source#file_old#_get_oldfiles",
-          )) as string[];
+        const oldfiles = await args.denops.call(
+          "ddu#source#file_old#_get_oldfiles",
+        ) as string[];
 
         controller.enqueue(oldfiles.map((f) => ({
           word: f.startsWith(cwd) ? relative(cwd, f) : f,
