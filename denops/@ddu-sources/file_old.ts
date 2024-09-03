33ac9f3910f805e6f4eef97851da931be2ee1c09
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-09-03 19:20:58 +0900

    Update deps

--- denops/@ddu-sources/file_old.ts
+++ denops/@ddu-sources/file_old.ts
@@ -1,11 +1,12 @@
-import { BaseSource, type Item } from "jsr:@shougo/ddu-vim@^5.0.0/types";
+import { type Item } from "jsr:@shougo/ddu-vim@~6.1.0/types";
+import { BaseSource } from "jsr:@shougo/ddu-vim@~6.1.0/source";
 
-import { type ActionData } from "jsr:@shougo/ddu-kind-file@^0.8.0";
+import { type ActionData } from "jsr:@shougo/ddu-kind-file@~0.9.0";
 
-import type { Denops } from "jsr:@denops/core@^7.0.0";
-import * as fn from "jsr:@denops/std@^7.0.1/function";
+import type { Denops } from "jsr:@denops/core@~7.0.0";
+import * as fn from "jsr:@denops/std@~7.1.0/function";
 
-import { relative } from "jsr:@std/path@1.0.2";
+import { relative } from "jsr:@std/path@~1.0.3/relative";
 
 type Params = Record<string, never>;
 
